9603134b80c288c97ab270e13610c3086fd88839
WizrdJim <wzrdjim@gmail.com>
2017-08-22 13:24:05 -0400

    Longest Run done

--- fizzBuzzTesting/fizzBuzz.js
+++ fizzBuzzTesting/fizzBuzz.js
@@ -9,6 +9,13 @@
  */
 
 const fizzBuzz = (num) => {
-
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzBuzz';
+  } else if (num % 3 === 0) {
+    return 'fizz'
+  } else if (num % 5 === 5) {
+    return 'buzz'
+  }
+  return num;
 };
 
--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -8,3 +8,21 @@
 * Input: "abcdefgh" Output: [ 0, 0 ]
 * Input: "aabbbcccc" Output: [ 5, 4 ]
 */
+
+const longestRun = (string) => {
+  const input = string;
+  let curr = [0,0];
+  let max = [0,0];
+  let compare = input.charAt(0);
+  lrObj = {};
+  //loop through the string
+  for (let i = 1; i < input.length; i++) {
+    if (input[i -1] === input[i]) {
+      curr[1] = i;
+      if (curr[1] - curr[0] > max[1] - max[0]){
+        max = curr;
+      }
+    } else curr = [i, i];
+  }
+  return max;
+}
