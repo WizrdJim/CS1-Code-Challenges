5ac9987da4882941f3cc728dad5bb55c4a331841
WizrdJim <wzrdjim@gmail.com>
2017-08-24 13:23:02 -0400

    breadthFirstSearch notes added

--- breadthFirstSearch/breadthFirstSearch.js
+++ breadthFirstSearch/breadthFirstSearch.js
@@ -16,7 +16,22 @@
  * breadthFirstSearch(tree, 2);// will return true before it recursively searches `z`
  */
 
-const breadthFirstSearch = (tree, 2) => {
-
-
+const breadthFirstSearch = (tree, searchTerm) => {
+  //research Object. properties!!!
+  //define queue as the child values in the tree
+  let queue = Object.values(tree);
+  //check if values are in the queue
+  while (queue.length > 0) {
+    // makes value the first thing in the queue
+    const value = queue.shift();
+    // Win condition
+    if (value === searchTerm) return true;
+    // Checks to make sure that null was not received and that its not getting an array
+    if(typeof value === 'object' && value !== null && !Array.isArray(value)){
+      // Inputs the children of the value into the queue
+      queue = queue.concat(Object.values(value));
+    }
+    // Never find then Lose
+    return false;
+  }
 }
